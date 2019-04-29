473f5ec68aadbbab7bdec91a7578003f8d359de6
yin_hang <pigbayspy@gmail.com>
2019-04-29 22:32:55 +0800

    重构代码结构

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import urllib.request
 from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY, date2num
@@ -10,6 +9,7 @@ import json
 
 
 def import_data(ticker, timeseries):
+    url, name = None, None
     if timeseries == 'intraday':
         # intraday 指的是一天内的交易数据
         url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + ticker + '&interval=1min&apikey=' + '9JQS102QXL5THDIL' + '&outputsize=full&datatype=json'
@@ -21,7 +21,6 @@ def import_data(ticker, timeseries):
     fp = urllib.request.urlopen(url)
     mybytes = fp.read()
     js = mybytes.decode("utf8")
-    #     print(js)
     fp.close()
     parsed_data = json.loads(js)  # 读取json数据并且将json转化成字典
     ps = parsed_data[name]
@@ -30,43 +29,9 @@ def import_data(ticker, timeseries):
     return df
 
 
-apple = import_data('AAPL', 'daily')  # 'GOOGL'/'GOOG'; 'MSFT'
-cols = apple.columns
-apple[cols] = apple[cols].apply(pd.to_numeric, errors='coerce')  # 将每一列转化成数字变量
-apple.index = pd.to_datetime(apple.index, format='%Y-%m-%d')  # 将索引转化成时间变量
-plt.figure(figsize=(10, 5))  # 改变图片大小
-apple["Close"].plot(grid=True)
-plt.title('Close Price of AAPL')
-plt.show()
-
-mondays = WeekdayLocator(MONDAY)  # 用于横坐标设定，每个显示的横坐标都是周一
-alldays = DayLocator()  # 横坐标的最小单位为一天
-
-fig, ax = plt.subplots()
-fig.subplots_adjust(bottom=0.2)
-weekFormatter = DateFormatter('%b %d, %Y')  # 设置坐标显示的日期格式Jun 5, 2018
-ax.xaxis.set_major_locator(mondays)
-ax.xaxis.set_minor_locator(alldays)
-ax.xaxis.set_major_formatter(weekFormatter)
-ax.grid(True)
-candlestick_ohlc(ax, list(zip(list(date2num(apple.index.tolist())), apple["Open"].tolist(), apple["High"].tolist(),
-                              apple["Low"].tolist(), apple["Close"].tolist())),
-                 colorup="red", colordown="green", width=1 * .4)
-
-ax.xaxis_date()
-ax.autoscale_view()
-plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
-plt.title('AAPL')
-plt.show()
-
-dt = date2num(date(2019, 4, 9))
-ax.annotate(str(apple.loc['2019-04-09', 'High']), xy=(dt - 2, 175))
-ax.annotate(str(apple.loc['2019-04-09', 'Low']), xy=(dt - 2, 168))
-
-
-def pandas_candlestick_ohlc(dat, ticker, lag="day", label_date=None, x1=0, x2=0):
+def pandas_candlestick_ohlc(dat, ticker, lag="day", label_date=None, x1=0, x2=0, apple=None):
     mondays = WeekdayLocator(MONDAY)
-    alldays = DayLocator()
+    all_days = DayLocator()
 
     transdat = dat.loc[:, ["Open", "High", "Low", "Close"]]
     if type(lag) == str:
@@ -98,7 +63,7 @@ def pandas_candlestick_ohlc(dat, ticker, lag="day", label_date=None, x1=0, x2=0)
     fig.subplots_adjust(bottom=0.2)
     weekFormatter = DateFormatter('%b %d, %Y')
     ax.xaxis.set_major_locator(mondays)
-    ax.xaxis.set_minor_locator(alldays)
+    ax.xaxis.set_minor_locator(all_days)
     ax.xaxis.set_major_formatter(weekFormatter)
     ax.grid(True)
 
@@ -118,11 +83,43 @@ def pandas_candlestick_ohlc(dat, ticker, lag="day", label_date=None, x1=0, x2=0)
     plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
 
     plt.show()
-    pandas_candlestick_ohlc(apple.loc['2019-01-26':'2018-04-26', :], ticker='AAPL', lag='day', label_date='2018-04-09', x1=175, x2=168)
 
 
 def main():
-    pass
+    apple = import_data('AAPL', 'daily')  # 'GOOGL'/'GOOG'; 'MSFT'
+    cols = apple.columns
+    apple[cols] = apple[cols].apply(pd.to_numeric, errors='coerce')  # 将每一列转化成数字变量
+    apple.index = pd.to_datetime(apple.index, format='%Y-%m-%d')  # 将索引转化成时间变量
+    plt.figure(figsize=(10, 5))  # 改变图片大小
+    apple["Close"].plot(grid=True)
+    plt.title('Close Price of AAPL')
+    plt.show()
+
+    mondays = WeekdayLocator(MONDAY)  # 用于横坐标设定，每个显示的横坐标都是周一
+    alldays = DayLocator()  # 横坐标的最小单位为一天
+
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(bottom=0.2)
+    weekFormatter = DateFormatter('%b %d, %Y')  # 设置坐标显示的日期格式Jun 5, 2018
+    ax.xaxis.set_major_locator(mondays)
+    ax.xaxis.set_minor_locator(alldays)
+    ax.xaxis.set_major_formatter(weekFormatter)
+    ax.grid(True)
+    candlestick_ohlc(ax, list(zip(list(date2num(apple.index.tolist())), apple["Open"].tolist(), apple["High"].tolist(),
+                                  apple["Low"].tolist(), apple["Close"].tolist())),
+                     colorup="red", colordown="green", width=1 * .4)
+
+    ax.xaxis_date()
+    ax.autoscale_view()
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.title('AAPL')
+    plt.show()
+
+    dt = date2num(date(2019, 4, 9))
+    ax.annotate(str(apple.loc['2019-04-09', 'High']), xy=(dt - 2, 175))
+    ax.annotate(str(apple.loc['2019-04-09', 'Low']), xy=(dt - 2, 168))
+
+    pandas_candlestick_ohlc(apple.loc['2019-01-26':'2018-04-26', :], ticker='AAPL', lag='day', label_date='2018-04-09', x1=175, x2=168, apple=apple)
 
 
 if __name__ == '__main__':
